8f264adbde881bf9280bb56ceedf206285dbf287
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-03-02 16:48:38 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>WP Theme N Two</title>
+  <meta name="description" content="N-Two">
+  <meta name="author" content="SitePoint">
+
+  <link rel="stylesheet" href="styles.css">
+
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <h1>Theme N two</h1>
+                <div class="header-right">
+                    <form>
+                        <input type="text" placeholder="Search">
+                    </form>
+                </div>
+        </div>
+    </header>
+
+
+</body>
+</html>
