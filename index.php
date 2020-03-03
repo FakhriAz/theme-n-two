e4d253d63301395615ba242ea0f75edd70f9a0ac
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-03-03 17:11:48 +0100

    Added the HTML basic index layout

--- index.php
+++ index.php
@@ -23,7 +23,64 @@
                 </div>
         </div>
     </header>
+    <nav class="nav main-nav">
+        <div class="container">
+            <ul>
+                <li><a href="Home">Home</a></li>
+                <li><a href="About">About</a></li>
+                <li><a href="#"></a>Services</li>
+            </ul>
+        </div>
+    </nav>
 
+    <div class="container content">
+        <div class="main block">
+            <article class="post">
+                <h2>Blog post 1</h2>
+                <p class="meta">Posted 11:00am 9 April 2020 By admin</p>
+                <p>SpaceX is planning a huge boost to the number of rocket launches from its Florida launch sites in the next few years as the company builds its Starlink satellite megaconstellation while meeting flight demands from its customers, according to a federal environmental report. 
 
+The missions for SpaceX's Falcon 9 and Falcon Heavy rockets will also have more options than in the past, according to the report, which was first reported by SpaceNews. One change will be a new mobile service tower allowing some missions to be assembled vertically, rather than horizontally. Another will be the capability to launch to polar orbits — quite the feat, since Florida is located close to the equator and better optimized for missions that operate close to the equator. SpaceX also plans to test recovering payload fairings as the company pushes for greater mission reusability.
+
+By 2023, the company wants to launch 70 missions a year from its two Florida launch sites at the Kennedy Space Center and nearby Cape Canaveral Air Force Station, using Falcon 9 and Falcon Heavy rockets. This rate is a seven-fold increase from the 11 missions SpaceX put into orbit in 2019, and almost double the 38 planned launches in 2020. That information comes from a draft environmental assessment published Thursday (Feb. 27) by the Federal Aviation Administration's Office of Commercial Space Transportation.</p>
+            <a class="botton" href="#">Read More</a>
+            </article>
+            <article class="post">
+                <h2>Blog post 2</h2>
+                <p class="meta">Posted 11:00am 9 April 2020 By admin</p>
+                <p>SpaceX is planning a huge boost to the number of rocket launches from its Florida launch sites in the next few years as the company builds its Starlink satellite megaconstellation while meeting flight demands from its customers, according to a federal environmental report. 
+
+The missions for SpaceX's Falcon 9 and Falcon Heavy rockets will also have more options than in the past, according to the report, which was first reported by SpaceNews. One change will be a new mobile service tower allowing some missions to be assembled vertically, rather than horizontally. Another will be the capability to launch to polar orbits — quite the feat, since Florida is located close to the equator and better optimized for missions that operate close to the equator. SpaceX also plans to test recovering payload fairings as the company pushes for greater mission reusability.
+
+By 2023, the company wants to launch 70 missions a year from its two Florida launch sites at the Kennedy Space Center and nearby Cape Canaveral Air Force Station, using Falcon 9 and Falcon Heavy rockets. This rate is a seven-fold increase from the 11 missions SpaceX put into orbit in 2019, and almost double the 38 planned launches in 2020. That information comes from a draft environmental assessment published Thursday (Feb. 27) by the Federal Aviation Administration's Office of Commercial Space Transportation.</p>
+            <a class="botton" href="#">Read More</a>
+            </article>
+            <article class="post">
+                <h2>Blog post 3</h2>
+                <p class="meta">Posted 11:00am 9 April 2020 By admin</p>
+                <p>SpaceX is planning a huge boost to the number of rocket launches from its Florida launch sites in the next few years as the company builds its Starlink satellite megaconstellation while meeting flight demands from its customers, according to a federal environmental report. 
+
+The missions for SpaceX's Falcon 9 and Falcon Heavy rockets will also have more options than in the past, according to the report, which was first reported by SpaceNews. One change will be a new mobile service tower allowing some missions to be assembled vertically, rather than horizontally. Another will be the capability to launch to polar orbits — quite the feat, since Florida is located close to the equator and better optimized for missions that operate close to the equator. SpaceX also plans to test recovering payload fairings as the company pushes for greater mission reusability.
+
+By 2023, the company wants to launch 70 missions a year from its two Florida launch sites at the Kennedy Space Center and nearby Cape Canaveral Air Force Station, using Falcon 9 and Falcon Heavy rockets. This rate is a seven-fold increase from the 11 missions SpaceX put into orbit in 2019, and almost double the 38 planned launches in 2020. That information comes from a draft environmental assessment published Thursday (Feb. 27) by the Federal Aviation Administration's Office of Commercial Space Transportation.</p>
+            <a class="botton" href="#">Read More</a>
+            </article>
+        </div>
+    </div>
+    <footer class="main-footer">
+        <div class="container">
+            <div class="footer_left">
+                <p>&copy; 2020 Fakhri Azzouz</p>
+            </div>  
+            <div class="footer_left">
+            <ul>
+                <li><a href="Home">Home</a></li>
+                <li><a href="About">About</a></li>
+                <li><a href="#"></a>Services</li>
+            </ul>
+            </div>       
+        </div>
+        
+    </footer>
 </body>
 </html>
