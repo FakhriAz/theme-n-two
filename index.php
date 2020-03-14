50fa4fd5b444f8dbf277175ec8d7b80d492832c2
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-03-14 16:39:32 +0100

    Minor changes: post_class() & etc...

--- index.php
+++ index.php
@@ -19,7 +19,7 @@
         <div class="container__header">
             <div class="header-1">
                 <h1>
-                    <a href="index.php">
+                    <a href="<?php echo home_url('/'); ?>">
                         <?php bloginfo('name'); ?>
                     </a>  
                     <small><?php bloginfo('description'); ?></small>
@@ -46,36 +46,26 @@
 
     <div class="container-content">
         <div class="main block">
-            <article class="post">
-                <h2>Blog post 1</h2>
-                <p class="meta">Posted 11:00am 9 April 2020 By admin</p>
-                <p>SpaceX is planning a huge boost to the number of rocket launches from its Florida launch sites in the next few years as the company builds its Starlink satellite megaconstellation while meeting flight demands from its customers, according to a federal environmental report. 
-
-The missions for SpaceX's Falcon 9 and Falcon Heavy rockets will also have more options than in the past, according to the report, which was first reported by SpaceNews. One change will be a new mobile service tower allowing some missions to be assembled vertically, rather than horizontally. Another will be the capability to launch to polar orbits — quite the feat, since Florida is located close to the equator and better optimized for missions that operate close to the equator. SpaceX also plans to test recovering payload fairings as the company pushes for greater mission reusability.
-
-By 2023, the company wants to launch 70 missions a year from its two Florida launch sites at the Kennedy Space Center and nearby Cape Canaveral Air Force Station, using Falcon 9 and Falcon Heavy rockets. This rate is a seven-fold increase from the 11 missions SpaceX put into orbit in 2019, and almost double the 38 planned launches in 2020. That information comes from a draft environmental assessment published Thursday (Feb. 27) by the Federal Aviation Administration's Office of Commercial Space Transportation.</p>
-            <a class="botton__post" href="#">Read More</a>
+            <article class="posts">
+                <?php if(have_posts()) : ?>
+                    <?php while (have_posts()) : the_post(); ?>
+                        <h2><?php the_title(); ?></h2>
+                        <p class="meta">
+                            Posted at
+                            <?php the_time('F j, Y g: i a'); ?>
+                            By
+                            <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>"> 
+                            <?php the_author(); ?>
+                            <?php the_category( ' ' ); ?>
+                            </a>    
+                            </p>
+                        <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>><?php the_excerpt(); ?></div>
+                <a class="botton__post" href="#">Read More</a>
             </article>
-            <article class="post">
-                <h2>Blog post 2</h2>
-                <p class="meta">Posted 11:00am 9 April 2020 By admin</p>
-                <p>SpaceX is planning a huge boost to the number of rocket launches from its Florida launch sites in the next few years as the company builds its Starlink satellite megaconstellation while meeting flight demands from its customers, according to a federal environmental report. 
-
-The missions for SpaceX's Falcon 9 and Falcon Heavy rockets will also have more options than in the past, according to the report, which was first reported by SpaceNews. One change will be a new mobile service tower allowing some missions to be assembled vertically, rather than horizontally. Another will be the capability to launch to polar orbits — quite the feat, since Florida is located close to the equator and better optimized for missions that operate close to the equator. SpaceX also plans to test recovering payload fairings as the company pushes for greater mission reusability.
-
-By 2023, the company wants to launch 70 missions a year from its two Florida launch sites at the Kennedy Space Center and nearby Cape Canaveral Air Force Station, using Falcon 9 and Falcon Heavy rockets. This rate is a seven-fold increase from the 11 missions SpaceX put into orbit in 2019, and almost double the 38 planned launches in 2020. That information comes from a draft environmental assessment published Thursday (Feb. 27) by the Federal Aviation Administration's Office of Commercial Space Transportation.</p>
-            <a class="botton__post" href="#">Read More</a>
-            </article>
-            <article class="post">
-                <h2>Blog post 3</h2>
-                <p class="meta">Posted 11:00am 9 April 2020 By admin</p>
-                <p>SpaceX is planning a huge boost to the number of rocket launches from its Florida launch sites in the next few years as the company builds its Starlink satellite megaconstellation while meeting flight demands from its customers, according to a federal environmental report. 
-
-The missions for SpaceX's Falcon 9 and Falcon Heavy rockets will also have more options than in the past, according to the report, which was first reported by SpaceNews. One change will be a new mobile service tower allowing some missions to be assembled vertically, rather than horizontally. Another will be the capability to launch to polar orbits — quite the feat, since Florida is located close to the equator and better optimized for missions that operate close to the equator. SpaceX also plans to test recovering payload fairings as the company pushes for greater mission reusability.
-
-By 2023, the company wants to launch 70 missions a year from its two Florida launch sites at the Kennedy Space Center and nearby Cape Canaveral Air Force Station, using Falcon 9 and Falcon Heavy rockets. This rate is a seven-fold increase from the 11 missions SpaceX put into orbit in 2019, and almost double the 38 planned launches in 2020. That information comes from a draft environmental assessment published Thursday (Feb. 27) by the Federal Aviation Administration's Office of Commercial Space Transportation.</p>
-            <a class="botton__post" href="#">Read More</a>
-            </article>
+                <?php endwhile; ?>
+            <?php else : ?>
+                    <?php echo apautop('We did no find what you are looking for.')?>
+            <?php endif; ?>
         </div>
         <div class="sidebar">
                     <div class="sidebar__block">
