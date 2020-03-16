b1acefddc2b8fb25f3c3d24c333b9b57cc921cb6
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-03-16 18:06:36 +0100

    Template partial added

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+        <div class="container__footer">
+            <ul>
+                <li><p>&copy; 2020 Fakhri Azzouz</p></li>
+                <li><a href="Home">Home</a></li>
+                <li><a href="About">About</a></li>
+                <li><a href="Services">Services</a></li>
+            </ul>
+        </div>
+    </footer>
+    <?php wp_footer(); ?>
+</body>
+</html>
