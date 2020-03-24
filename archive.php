bf9441f0308506a6f0140516a796c36f1a927ba5
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-03-24 16:29:11 +0100

    Reverted back to previous versions.

--- archive.php
+++ archive.php
@@ -17,7 +17,7 @@
                              echo "Archive's Author: " . get_the_author();
                              rewind_posts(); 
                         }
-                         elseif ( is_day() ) {
+                        elseif ( is_day() ) {
                             echo 'Daily Archives: ' . get_the_date();
                         } elseif ( is_month() ) {
                             echo 'Monthly Archives: ' . get_the_date( 'F Y' );
@@ -26,6 +26,7 @@
                         } else {
                             echo 'Archives: ';
                         }
+
                     ?>
                     <?php while (have_posts()) : the_post(); ?>
                         <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>></div>
@@ -53,12 +54,13 @@
             <?php endif; ?>
         </div>
         <div class="sidebar">
-                <!-- Top right sidebar widget start -->
-                <?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
-                <?php dynamic_sidebar( 'sidebar1' ); ?> 
-                <?php else : ?>
-                <!-- Time to add some widgets! -->
-            <?php endif; ?>
-        </div>
+                    <div class="sidebar__block">
+                        <p>SpaceX is planning a huge boost to the number of rocket launches from its
+                         Florida launch sites in the next few years as the company builds
+                         its Starlink satellite megaconstellation while meeting flight demands
+                         from its customers, according to a federal environmental report.</p>
+                         <a class="botton"></a>
+                    </div>
+                </div>
     </div>
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -14,16 +14,4 @@
     add_action('after_setup_theme', 'n_two_theme_support');
 
 
-    // Widgets
-    function n_two_init_widgets($id) {
-        register_sidebar(array (
-            'name'  => 'Archive Sidebar',
-            'id'    => 'sidebar1',
-            'before_widget' => '<div class="posts-archive-container">',
-            'after_widget' => '</div>',
-            'before_title' => '<h3 class="posts-archive-title">',
-            'after_title' => '</h3>'
-        ));
-    }
-    add_action( 'widgets_init', 'n_two_init_widgets' );
 ?>
