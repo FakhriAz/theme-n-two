e9b01fa5d9a0f89f8881a06a6ef9c20cb832f18a
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-04-26 17:53:29 +0200

    page.php

--- page.php
+++ page.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
-        
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <article class="page">
+        <h3><?php the_title(); ?></h3>
+            <?php the_content(); ?>
+        </article>
+        <?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 <?php get_footer(); ?>  
