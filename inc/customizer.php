c782266c5ab2a698ed4b7b7cea37ccbe21a02bbd
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-05-13 14:23:17 +0200

    Updates in name, priorities, and add more control and setting to the images in col 1

--- inc/customizer.php
+++ inc/customizer.php
@@ -4,7 +4,7 @@
     $wp_customize->add_section('showcase_2', array(
       'title'   => __('Showcase Column 2 Image Cover', 'n_two'),
       'description' => sprintf(__('Options for showcase_2','n_two')),
-      'priority'    => 140
+      'priority'    => 150
     ));
 
     $wp_customize->add_setting('showcase_2_image', array(
@@ -20,10 +20,11 @@
     )));
 
         // 3 images in col 1 start 
-    $wp_customize->add_section('showcase_1', array(
-      'title'   => __('Showcase Column 1 Text Only', 'n_two'),
+    
+    $wp_customize->add_section('showcase_1_img', array(
+      'title'   => __('Showcase Column 1 images ', 'n_two'),
       'description' => sprintf(__('Options for showcase','n_two')),
-      'priority'    => 135
+      'priority'    => 140,
     ));
 
     $wp_customize->add_setting('small_image1', array(
@@ -33,10 +34,82 @@
 
     $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'small_image1', array(
       'label'   => __('Rounded image 1', 'n_two'),
-      'section' => 'showcase_1',
+      'section' => 'showcase_1_img',
       'settings' => 'small_image1',
-      'priority'  => 2
+      'priority'  => 3
     )));
+
+    $wp_customize->add_setting('showcase_1img1_text', array(
+      'default'   => _x('Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam', 'n_two'),
+      'type'      => 'theme_mod'
+    ));
+
+    $wp_customize->add_control('showcase_1img1_text', array(
+      'label'   => __('Text', 'n_two'),
+      'section' => 'showcase_1_img',
+      'priority'  => 4
+    ));
+    // img 1 & text 1 done
+    
+    //Image 2 & text 2 starts
+    $wp_customize->add_setting('small_image2', array(
+      'default'   => get_bloginfo('template_directory').'/img/lunch-2.jpg',
+      'type'      => 'theme_mod'
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'small_image2', array(
+      'label'   => __('Rounded image 2', 'n_two'),
+      'section' => 'showcase_1_img',
+      'settings' => 'small_image2',
+      'priority'  => 5
+    )));
+
+    $wp_customize->add_setting('showcase_2img2_text', array(
+      'default'   => _x('Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam', 'n_two'),
+      'type'      => 'theme_mod'
+    ));
+
+    $wp_customize->add_control('showcase_2img2_text', array(
+      'label'   => __('Text', 'n_two'),
+      'section' => 'showcase_1_img',
+      'priority'  => 6
+    ));
+    // image 2 & text done
+
+    // Image 3 & text 3 starts
+    $wp_customize->add_setting('small_image3', array(
+      'default'   => get_bloginfo('template_directory').'/img/lunch-3.jpg',
+      'type'      => 'theme_mod'
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'small_image3', array(
+      'label'   => __('Rounded image 3', 'n_two'),
+      'section' => 'showcase_1_img',
+      'settings' => 'small_image3',
+      'priority'  => 7
+    )));
+
+    $wp_customize->add_setting('showcase_3img3_text', array(
+      'default'   => _x('Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam', 'n_two'),
+      'type'      => 'theme_mod'
+    ));
+
+    $wp_customize->add_control('showcase_3img3_text', array(
+      'label'   => __('Text', 'n_two'),
+      'section' => 'showcase_1_img',
+      'priority'  => 8
+    ));
+
+    // img 3 & text 3 done
+
+
+    // 3 images in col1 & text ends here
+    
+    $wp_customize->add_section('showcase_1', array(
+      'title'   => __('Showcase Column 1 Text Only', 'n_two'),
+      'description' => sprintf(__('Options for showcase','n_two')),
+      'priority'    => 135
+    ));
 
     $wp_customize->add_setting('showcase_1_heading', array(
       'default'   => _x('Custom Wordpress Theme', 'n_two'),
