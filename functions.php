0d7d7c79eb12ff1156e5e2bf5ea53702015cd9ca
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-05-27 11:04:20 +0200

    Code formatting

--- functions.php
+++ functions.php
@@ -1,23 +1,23 @@
 <?php
-    // Theme support
-    function n_two_theme_support() {
-        //Support for featured image
-        add_theme_support('post-thumbnails');
+// Theme support
+function n_two_theme_support()
+{
+    //Support for featured image
+    add_theme_support('post-thumbnails');
 
-        // Nav menus    
-        register_nav_menus(array(
-            'primary'  => __('Primary Menu'),
-            'footer'   => __('Footer Menu'), 
-        ));
+    // Nav menus    
+    register_nav_menus(array(
+        'primary'  => __('Primary Menu'),
+        'footer'   => __('Footer Menu'),
+    ));
 
-        // Posts format support
-        add_theme_support('post-formats', array('aside', 'link','status'));
-        // add_post_type_support( 'post', 'post-formats' );
-        // add_post_type_support( 'page', 'post-formats' );
-    }
+    // Posts format support
+    add_theme_support('post-formats', array('aside', 'link', 'status'));
+    // add_post_type_support( 'post', 'post-formats' );
+    // add_post_type_support( 'page', 'post-formats' );
+}
 
-    add_action('after_setup_theme', 'n_two_theme_support');
+add_action('after_setup_theme', 'n_two_theme_support');
 
-    // Customizer File
-    require get_template_directory(). '/inc/customizer.php';
-?>
+// Customizer File
+require get_template_directory() . '/inc/customizer.php';
