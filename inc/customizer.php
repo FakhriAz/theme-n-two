334cdc7f2ae3cbf3ba6ecbfb098b1241580cfc66
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-06-19 17:15:52 +0200

    Reordering code as shown in customizer and basic code update

--- inc/customizer.php
+++ inc/customizer.php
@@ -1,24 +1,37 @@
 <?php
 function n_two_customize_register($wp_customize)
 {
-  // Showcase Section
-  $wp_customize->add_section('showcase_2', array(
-    'title'   => __('Showcase Column 2 Image Cover', 'n_two'),
-    'description' => sprintf(__('Options for showcase_2', 'n_two')),
-    'priority'    => 150
+  //Column 1 text only starts 
+
+  $wp_customize->add_section('showcase_1', array(
+    'title'   => __('Showcase Column 1 Text Only', 'n_two'),
+    'description' => sprintf(__('Options for showcase', 'n_two')),
+    'priority'    => 135
   ));
 
-  $wp_customize->add_setting('showcase_2_image', array(
-    'default'   => get_bloginfo('template_directory') . '/img/showcase.jpg',
+  $wp_customize->add_setting('showcase_1_heading', array(
+    'default'   => _x('Custom Wordpress Theme', 'n_two'),
     'type'      => 'theme_mod'
   ));
 
-  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'showcase_image', array(
-    'label'   => __('Showcase Image', 'n_two'),
-    'section' => 'showcase_2',
-    'settings' => 'showcase_2_image',
-    'priority'  => 1
-  )));
+  $wp_customize->add_control('showcase_1_heading', array(
+    'label'   => __('Heading', 'n_two'),
+    'section' => 'showcase_1',
+    'priority'  => 3
+  ));
+
+  $wp_customize->add_setting('showcase_1_text', array(
+    'default'   => _x('Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam', 'n_two'),
+    'type'      => 'theme_mod'
+  ));
+
+  $wp_customize->add_control('showcase_1_text', array(
+    'label'   => __('Text', 'n_two'),
+    'section' => 'showcase_1',
+    'priority'  => 4
+  ));
+
+  //Column 1 text only ends here
 
   // 3 images in col 1 start 
 
@@ -102,39 +115,27 @@ function n_two_customize_register($wp_customize)
   ));
 
   // img 3 & text 3 done
-
-
   // 3 images in col1 & text ends here
 
-  $wp_customize->add_section('showcase_1', array(
-    'title'   => __('Showcase Column 1 Text Only', 'n_two'),
-    'description' => sprintf(__('Options for showcase', 'n_two')),
-    'priority'    => 135
+  // Showcase cover image in column 2 container 1
+  $wp_customize->add_section('showcase_2', array(
+    'title'   => __('Showcase Column 2 Image Cover', 'n_two'),
+    'description' => sprintf(__('Options for showcase_2', 'n_two')),
+    'priority'    => 150
   ));
 
-  $wp_customize->add_setting('showcase_1_heading', array(
-    'default'   => _x('Custom Wordpress Theme', 'n_two'),
+  $wp_customize->add_setting('showcase_2_image', array(
+    'default'   => get_bloginfo('template_directory') . '/img/showcase.jpg',
     'type'      => 'theme_mod'
   ));
 
-  $wp_customize->add_control('showcase_1_heading', array(
-    'label'   => __('Heading', 'n_two'),
-    'section' => 'showcase_1',
-    'priority'  => 3
-  ));
+  $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'showcase_image', array(
+    'label'   => __('Showcase Image', 'n_two'),
+    'section' => 'showcase_2',
+    'settings' => 'showcase_2_image',
+    'priority'  => 1
+  )));
 
-  $wp_customize->add_setting('showcase_1_text', array(
-    'default'   => _x('Sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean eu leo quam', 'n_two'),
-    'type'      => 'theme_mod'
-  ));
-
-  $wp_customize->add_control('showcase_1_text', array(
-    'label'   => __('Text', 'n_two'),
-    'section' => 'showcase_1',
-    'priority'  => 4
-  ));
-
-  // 3 images in col 1 end
 
   // Start panel 1 with button & text only
 
@@ -166,7 +167,6 @@ function n_two_customize_register($wp_customize)
     'priority'  => 6
   ));
 
-
   // Panel 1 with text & button ends here
 
   // Container 3 image cover starts
@@ -194,7 +194,7 @@ function n_two_customize_register($wp_customize)
   // Container 3 text heading starts
 
   $wp_customize->add_section('showcase 3 container 3', array(
-    'title'   => __('Showcase 3 Text Heading & Text', 'n_two'),
+    'title'   => __('Showcase 3 Heading & Text', 'n_two'),
     'description' => sprintf(__('Options for showcase', 'n_two')),
     'priority'    => 190
   ));
