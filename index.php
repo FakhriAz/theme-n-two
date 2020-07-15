4745ab231a990075342e649b40a5c79959f3516d
Fakhri Azzouz <azzouz.fakhri@gmail.com>
2020-07-15 09:54:35 +0200

    HTML class name fix Pagination added Closing div tag added

--- index.php
+++ index.php
@@ -3,13 +3,15 @@
     </header>
 
     <div class="container-content">
-        <div class="main block">
+        <div class="main-block">
         <?php if ( have_posts() ) :
              while ( have_posts() ) : the_post(); ?>
             <?php get_template_part('content', get_post_format()); ?> 
             <?php endwhile;
+            echo paginate_links();
              else : ?>
 	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
 <?php endif; ?> 
-    </div>
+        </div>
+    </div> <!-- Test closing div for container-content -->
 <?php get_footer(); ?>  
